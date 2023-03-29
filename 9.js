045b1b3ae3ea435a2b299f225668ed5d3f9a7bde
MarcosHolmes <93903818+MarcosHolmes@users.noreply.github.com>
2023-03-28 23:47:12 -0600

    9.- Introducir un número por teclado y que nos diga si es positivo o negativo.

---
+++ 9.js
@@ -0,0 +1,11 @@
+let n = prompt("Introduce un número: ");
+
+if (n > 0){
+    console.log("Es positivo");
+}
+else if(n < 0){
+    console.log("Es negativo");
+}
+else{
+    console.log("Es cero");
+}
