460b19fb0e101f05d8427d3a80acbb9e213345ca
MarcosHolmes <93903818+MarcosHolmes@users.noreply.github.com>
2023-04-04 11:16:21 -0600

    17.- Imprimir, contar y sumar los múltiplos de 2 que hay entre una serie de números, tal que el segundo sea mayor o igual que el primero.

---
+++ 17.js
@@ -0,0 +1,18 @@
+const n1 = parseInt(prompt("Ingresa el primer número: "));
+let sum = 0;
+let c = 0;
+
+while(n2 < n1){
+    const n2 = parseInt(prompt("Ingresa el segundo número. debe de ser mayor que el primero: "));
+}
+
+for(let i=n1+1; i<=n2; i++){
+    if(i % 2 == 0){
+        console.log(i);
+        c++;
+        sum = sum + i
+
+    }
+
+}
+
